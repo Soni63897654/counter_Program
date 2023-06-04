65254194a293e1de9a5f61e219e828eee7ce3628
Soni63897654 <118298882+Soni63897654@users.noreply.github.com>
2023-06-05 00:37:42 +0530

    counter_program

---
+++ app.js
@@ -0,0 +1,16 @@
+let counter = 0
+function increment(){
+	counter = counter + 1;
+	//conter++;
+	//counter += 1;
+	document.getElementById('counter').innerHTML = counter;
+}
+function decrement(){
+	counter = counter - 1;
+	document.getElementById('counter').innerHTML = counter;
+}
+function reset()
+{
+		counter = 0;
+		document.getElementById('counter').innerHTML = counter;
+}
